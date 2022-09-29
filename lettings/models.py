256b708ab9c5ed083b161d15a49fe2cb8e5c6347
Jérémy Bochard <jeremy.bochard@gmail.com>
2022-09-29 17:39:06 +0200

    split app

--- lettings/models.py
+++ lettings/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.db import models
 
@@ -26,11 +25,3 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
