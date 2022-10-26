17c86e3ec909064fc0f46a8ac92fe8fbb1dbe05f
Bochard Jeremy <jeremy.bochard@annalect.com>
2022-10-26 14:00:29 +0200

    use django_on_heroku

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,9 @@
 import os
 
+import django_on_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -13,7 +14,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "testocl4.herokuapp.com"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -110,3 +111,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+django_on_heroku.settings(locals())
