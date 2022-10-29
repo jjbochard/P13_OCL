213a5d64a3a0b6b449e9ec65cb7df26b3ca47d6a
Jérémy Bochard <jeremy.bochard@gmail.com>
2022-10-29 19:15:46 +0200

    fix allowed_hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "testocl4.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "oc-lettings-987.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
