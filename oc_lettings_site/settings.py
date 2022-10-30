896741a8643ff7e464579f7a9734d95c72caa398
Bochard Jeremy <jeremy.bochard@annalect.com>
2022-10-30 17:31:08 +0100

    add default env value

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = config("DJANGO_SECRET_KEY", default="foo")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
